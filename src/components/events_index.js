9e90409cfdb5923bc6839b2a78912576ab05155e
tomoya87 <lbeku22433@yahoo.co.jp>
2020-05-31 23:37:31 +0900

    implement events index

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import _ from 'lodash'
 
-import { increment, decrement } from '../actions'
+// import { readEvents } from '../actions'
+import { readEvents } from '../actions'
 // import PropTypes from 'prop-types';
 
 // class App extends Component {
@@ -31,26 +33,46 @@ import { increment, decrement } from '../actions'
 //   }
 // }
 
-class App extends Component {
+class EventsIndex extends Component {
+  componentDidMount() {
+    // console.log('Hi')
+    this.props.readEvents();
+  }
+  renderEvents() {
+    return _.map(this.props.events, event => (
+      <tr key={event.id}>
+        <td>{event.id}</td>
+        <td>{event.title}</td>
+        <td>{event.body}</td>
+      </tr>
+    ))
+  }
   render() {
-    const props = this.props
 
     return (
-      <React.Fragment>
-        <div>value: {props.value}</div>
-        <button onClick={props.increment}>+1</button>
-        <button onClick={props.decrement}>-1</button>
-      </React.Fragment>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Body</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {this.renderEvents()}
+        </tbody>
+      </table>
     )
   }
 }
 
-const mapStateToProps = state => ({ value: state.count.value })
+const mapStateToProps = state => ({ events: state.events })
 // const mapDispatchToProps = dispatch => ({
 //   increment: () => dispatch(increment()),
 //   decrement: () => dispatch(decrement())
 // })
 // ショートハンドの構文
-const mapDispatchToProps = ({ increment, decrement })
+const mapDispatchToProps = ({ readEvents })
 
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default connect(mapStateToProps, mapDispatchToProps)(EventsIndex)
