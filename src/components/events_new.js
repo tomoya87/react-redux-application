bc2c834e83d5e5761a7ec86eed5c73866cb207f1
tomoya87 <lbeku22433@yahoo.co.jp>
2020-06-01 00:54:06 +0900

    implement event new component

---
+++ src/components/events_new.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form'
+import { Link, useParams } from 'react-router-dom'
+
+import { postEvent } from '../actions'
+
+class EventsNew extends Component {
+  constructor(props) {
+    super(props)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+  renderField(field) {
+    const { input, label, type, meta: { touched, error } } = field
+
+    return (
+      <div>
+        <input {...input} placeholder={label} type={type} />
+        {touched && error && <span>{error}</span>}
+      </div>
+    )
+  }
+  async onSubmit(values) {
+    await this.props.postEvent(values)
+    this.props.history.push('/')
+  }
+  // pristine,submittingはsubmitを一度しか押せないようにする
+  render() {
+    const { handleSubmit, pristine, submitting } = this.props
+    // console.log(submitting)
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit)}>
+        <div><Field label="Title" name="title" type="text" component={this.renderField} /></div>
+        <div><Field label="Body" name="body" type="text" component={this.renderField} /></div>
+
+        <div>
+          <input type="submit" value="Submit" disabled={pristine || submitting} />
+          <Link to="/">Cancel</Link>
+        </div>
+      </form>
+    )
+  }
+}
+
+const validate = values => {
+  const errors = {}
+
+  if (!values.title) errors.title = "Enter a title please"
+  if (!values.body) errors.body = "Enter a body please"
+
+  return errors
+}
+const mapDipatchToProps = ({ postEvent })
+
+export default connect(null, mapDipatchToProps)(
+  reduxForm({ validate, form: 'eventNewForm' })(EventsNew)
+
+)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux'
+import { reducer as form } from 'redux-form'
 import events from './events'
 
-export default combineReducers({ events })
+export default combineReducers({ events, form })
 // このように複数くわえれる
 // export default combineReducers({ foo  ,bar ,baz})
